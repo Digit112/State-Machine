8f7f92f9eacbbad91f61050dbe6064b392901465
Evelyn Salie <ekobaddish@gmail.com>
2025-05-13 22:01:44 -0400

    Added function declarations and finalized enum declaration.

--- Graph.hpp
+++ Graph.hpp
@@ -1,5 +1,6 @@
 #include <vector>
 #include <memory>
+#include <utility>
 
 using namespace std {
 	template <class T, class U>
@@ -10,10 +11,18 @@ using namespace std {
 		
 		enum class Flags : unsigned int {
 			None = 0,
-			Symmetric = 1 << 0, // 
-			AntiSymmetric = 1 << 1, // 2
-			Transitive = 1 << 2, // 4
-			AntiTransitive = 1 << 2, // 4
+			Symmetric       = 1 <<  0, ///< Describes a graph which contains a reverse edge (f, s) for every edge (s, f).
+			AntiSymmetric   = 1 <<  1, ///< Describes a graph which does not contain the reverse edge (f, s) for any given edge (s, f).
+			Transitive      = 1 <<  2, ///< Describes a graph which contains an edge (a, b) between any two nodes for which a directed path from a to b would exist in the edge's absence.
+			AntiTransitive  = 1 <<  3, ///< Describes a graph which contains no edge (a, b) between two nodes for which a directed path from a to b would exist in the edge's absence.
+			Reflexive       = 1 <<  4, ///< Describes a graph containing an edge (a, a) for all nodes from themselves to themselves. A Reflexive graph is also not acyclic.
+			AntiReflexive   = 1 <<  6, ///< Describes a graph containing no edge (a, a) from any node to itself.
+			Connected       = 1 <<  6, ///< Describes a graph containing a directed path from any given node to any other. A connected graph is also weakly connected.
+			WeaklyConnected = 1 <<  7, ///< Describes a graph containing an undirected path between any two given nodes, a path that would be present in the undirected graph obtained by replacing all directed edges in this graph with undirected edges.
+			Acyclic         = 1 <<  8, ///< Describes a graph containg no directed paths from any node to itself. An Acyclic graph is also antireflexive.
+			Full            = 1 <<  9, ///< Describes a graph which contains an edge from any two given nodes. A full graph is also reflexive, symmetric, transitive, and connected.
+			Empty           = 1 << 10, ///< Describes a graph which contains no edges (but may contain nodes). An empty graph is symmetric, antisymmetric, transitive, antitransitive, antireflexive, and disconnected. It is also considered reflexive if there are no nodes.
+			Null            = 1 << 11, ///< Describes a graph with no nodes. A Null graph is also empty.
 		};
 		
 		// Define a class which connects nodes together.
@@ -59,8 +68,8 @@ using namespace std {
 		public:
 			Node(data) : data(T data) {}
 			
-			void set_data(T data); ///< Sets the datum associated with this edge.
-			T get_data(); ///< Retrieves the datum associated with this edge.
+			void set_data(T data); ///< Sets the datum associated with this node.
+			T get_data(); ///< Retrieves the datum associated with this node.
 			
 			/// Creates an edge from this node to the passed node.
 			/// Update's the passed node's incoming list as needed.
@@ -70,8 +79,16 @@ using namespace std {
 	
 	private:
 		vector<Node> nodes;
+		unsigned int num_edges;
 	
 	public:
+		Graph();
 		
+		Node add_node(T data);
+		
+		/// Adds an edge from the first node to the second and assigns the passed data to it.
+		/// If the edge already exists, simply assign the data and return the existing edge.
+		/// @return An edge connecting the first node to the second.
+		Edge add_edge(Node first, Node second, U data);
 	};
 }
